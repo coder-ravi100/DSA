7ad7aaacc0da5edc09dffcb4e33e2f4176014c49
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-25 13:57:25 +0530

    updates

--- Array/star.cpp
+++ Array/star.cpp
@@ -16,6 +16,14 @@ int main()
     {
         cout <<arr[i] <<" ";
     }
-    
     return 0;
 }
+/*How To Works Array
+i = 0 then 0 cond true print 0 = 10
+i = 1 then 1 cond true print 1 = 20
+i = 2 then 2 cond true print 2 = 30
+i = 3 then 3 cond true print 3 = 40
+i = 4 then 0 cond true print 4 = 50
+i = 5 then 5 cond flase statement is Execute
+Array 10 20 30 40 50
+*/
